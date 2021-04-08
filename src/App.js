280e5ed231c5e3cd99c998d19308a8d86a5e7343
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-04-08 10:30:56 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './components/home'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+
+    <>
+
+      <nav>
+          <ul>
+              <li>
+                  <Link to='/'>Home</Link>
+              </li>
+              <li>
+                  <Link to='/about'>About</Link>
+              </li>
+              <li>
+                  <Link to='/users'>Users</Link>
+              </li>
+          </ul>
+      </nav>
+    { /* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */ }
+      <Switch>
+
+        <Route path='/about'>
+          <About />
+        </Route>
+
+        <Route path='/users'>
+          <Users />
+        </Route>
+
+        <Route>
+          <Home />
+        </Route>
+
+      </Switch>
+
+    </>
+
+   </Router>
+
   );
+
+  
 }
 
-export default App;
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+ /* - home is at the end of the router and IN switch so if no path (previous links) is loaded,
+   it loads the one without defined path (home)
+   - without switch router would load every component with or without path, but with switch 
+   just the 1st one loads and the other ones don't  
+*/
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Home = () => {
+    return (
+<>
+<h1>Hello home!</h1>
+</>
+    )
+}
+
+export default Home
