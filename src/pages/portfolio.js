71dfe364b9987047a694434f47890669c4d383ac
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-09-22 16:39:37 +0200

    fixed links

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,6 +3,20 @@ import React from 'react';
 //assets
 import portfolio from './assets/portfoliodesigns/portfolio.pdf'
 
+//fullsizes
+import flowbook from './assets/portfoliodesigns/flowbook.png';
+import landingpage from './assets/portfoliodesigns/landingpage.png';
+import PrettyPuzzled2 from './assets/portfoliodesigns/PrettyPuzzled2.png';
+import tasteslikestrawberries2 from './assets/portfoliodesigns/tasteslikestrawberries2.png';
+import MachineManLayout from './assets/portfoliodesigns/MachineManLayout.png';
+import passionpages from './assets/portfoliodesigns/passionpages.png';
+import rollerskates from './assets/portfoliodesigns/rollerskates.png';
+import Furnessence from './assets/portfoliodesigns/Furnessence.png';
+import MyCalendar from './assets/portfoliodesigns/MyCalendar.png';
+import Bubbly from './assets/portfoliodesigns/Bubbly.png';
+import Happier from './assets/portfoliodesigns/Happier.png';
+import darkwillow from './assets/portfoliodesigns/darkwillow.png';
+import Licitar from './assets/portfoliodesigns/Licitar.png';
 //thumbs
 import thumbflowbook from './assets/portfoliodesigns/thumbflowbook.png';
 import thumblandingpage from './assets/portfoliodesigns/thumblandingpage.png';
@@ -35,29 +49,28 @@ const Portfolio = () => {
             <br/><br/>
 
                 <h2>Web design</h2>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/flowbook.png' target='_blank' rel='noreferrer'><img src={thumbflowbook} alt='flowbook' ></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/landingpage.png' target='_blank' rel='noreferrer'><img src={thumblandingpage} alt='landingpage' ></img></a>
-                {/*<a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/PrettyPuzzled2.png' target='_blank' rel='noreferrer'><img src={thumbPrettyPuzzled1} alt='prettypuzzled' ></img></a>*/}
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/PrettyPuzzled1.png' target='_blank' rel='noreferrer'><img src={thumbPrettyPuzzled2} alt='prettypuzzled' ></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/passionpages.png' target='_blank' rel='noreferrer'><img src={thumbpassionpages} alt='passionledushere'></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/rollerskates.png' target='_blank' rel='noreferrer'><img src={thumbrollerskates} alt='rollerskates'></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/Furnessence.png' target='_blank' rel='noreferrer'><img src={thumbFurnessence} alt='furnessence' ></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/tasteslikestrawberries2.png' target='_blank' rel='noreferrer'><img src={thumbtasteslikestrawberries2} alt='tasteslikestrawberries' ></img></a>
-                <a href='https://tasteslikestrawberries.github.io/machineman' target='_blank' rel='noreferrer' ><img src={thumbMachineManLayout} alt='machineman' style={{width:'initial'}}></img></a>
+                <a href={flowbook} target='_blank' rel='noreferrer'><img src={thumbflowbook} alt='flowbook' ></img></a>
+                <a href={landingpage} target='_blank' rel='noreferrer'><img src={thumblandingpage} alt='landingpage' ></img></a>
+                <a href={PrettyPuzzled2} target='_blank' rel='noreferrer'><img src={thumbPrettyPuzzled2} alt='prettypuzzled' ></img></a>
+                <a href={passionpages} target='_blank' rel='noreferrer'><img src={thumbpassionpages} alt='passionledushere'></img></a>
+                <a href={rollerskates} target='_blank' rel='noreferrer'><img src={thumbrollerskates} alt='rollerskates'></img></a>
+                <a href={Furnessence} target='_blank' rel='noreferrer'><img src={thumbFurnessence} alt='furnessence' ></img></a>
+                <a href={tasteslikestrawberries2} target='_blank' rel='noreferrer'><img src={thumbtasteslikestrawberries2} alt='tasteslikestrawberries' ></img></a>
+                <a href={MachineManLayout} target='_blank' rel='noreferrer' ><img src={thumbMachineManLayout} alt='machineman' style={{width:'initial'}}></img></a>
                 <a href='https://tasteslikestrawberries.github.io/layouts/monochromeparticles/shop.html' target='_blank' rel='noreferrer'><img src={thumbMonochromeParticlesLayout2} alt='monochromeparticles' style={{width:'initial'}}></img></a>
 
             <br/>
 
                 <h2>App design</h2>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/MyCalendar.png' target='_blank' rel='noreferrer'><img src={thumbMyCalendar} alt='bubbly'></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/Bubbly.png' target='_blank' rel='noreferrer'><img src={thumbBubbly} alt='bubbly'></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/Happier.png' target='_blank' rel='noreferrer'><img src={thumbHappier} alt='happier'></img></a>
+                <a href={MyCalendar} target='_blank' rel='noreferrer'><img src={thumbMyCalendar} alt='bubbly'></img></a>
+                <a href={Bubbly} target='_blank' rel='noreferrer'><img src={thumbBubbly} alt='bubbly'></img></a>
+                <a href={Happier} target='_blank' rel='noreferrer'><img src={thumbHappier} alt='happier'></img></a>
 
 
             <br/>
                 <h2>Other work</h2>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/darkwillow.png' target='_blank' rel='noreferrer'><img src={thumbdarkwillow} alt='darkwillow'></img></a>
-                <a href='https://tasteslikestrawberries.github.io/assets/portfoliodesigns/Licitar.png' target='_blank' rel='noreferrer'><img src={thumblicitar} alt='licitar'></img></a>
+                <a href={darkwillow} target='_blank' rel='noreferrer'><img src={thumbdarkwillow} alt='darkwillow'></img></a>
+                <a href={Licitar} target='_blank' rel='noreferrer'><img src={thumblicitar} alt='licitar'></img></a>
 
             </div>
 
